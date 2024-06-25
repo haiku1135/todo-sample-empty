2d96461814d4555d96a3591beef85a3655b6ff6d
takumi.takahashi <takumi.takahashi@gpol.co.jp>
2024-06-25 19:05:43 +0900

    [update]tailwindcssに慣れる

--- src/pages/practice/01.tsx
+++ src/pages/practice/01.tsx
@@ -1,7 +1,20 @@
 import { NextPage } from 'next';
 
+import ThreeBoxContent from '@/components/ThreeBoxContent';
+
 const Page: NextPage = () => {
-  return <div></div>;
+  return (
+    <div className="mx-auto mt-10 max-w-4xl">
+      <ThreeBoxContent
+        title1="これはタイトルです"
+        content1="これはコンテンツです"
+        title2="これはタイトルです"
+        content2="これはコンテンツです"
+        title3="これはタイトルです"
+        content3="これはコンテンツです"
+      />
+    </div>
+  );
 };
 
 export default Page;
